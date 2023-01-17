24c2c2584be6d50d31dafbfba202ae03decc7613
Renan Aguiar <renan.modesto@grupomultiplica.com.br>
2023-01-16 22:44:33 -0300

    ajuste api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 export const commingSoonMovies = axios.create({
-  baseURL: "https://api-appquarto237.up.railway.app/total",
+  // baseURL: "https://api-appquarto237.up.railway.app/total",
+  // baseURL: "http://localhost:3002/total",
+  baseURL: "https://api-quarto237.onrender.com/total",
 });
 
 export const MovieGetId = axios.create({
-  baseURL: "https://api-appquarto237.up.railway.app/",
+  // baseURL: "https://api-appquarto237.up.railway.app/",
+  // baseURL: "http://localhost:3002",
+  baseURL: "https://api-quarto237.onrender.com/",
 });
